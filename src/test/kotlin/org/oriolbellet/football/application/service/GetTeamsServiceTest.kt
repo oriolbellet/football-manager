404f219df37be24fc9d41387c99b26f12827ec15
Oriol Bellet <oriol.bellet@king.com>
2020-10-25 23:31:43 +0100

    Dockerized

--- src/test/kotlin/org/oriolbellet/football/application/service/GetTeamsServiceTest.kt
+++ src/test/kotlin/org/oriolbellet/football/application/service/GetTeamsServiceTest.kt
@@ -2,7 +2,10 @@ package org.oriolbellet.football.application.service
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.oriolbellet.football.adapter.out.TeamAdapter
+import org.oriolbellet.football.adapter.out.TeamDao
 import org.oriolbellet.football.application.port.out.FindTeam
+import org.oriolbellet.football.application.port.out.FindTeams
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
@@ -14,6 +17,12 @@ internal class GetTeamsServiceTest {
 
     @Autowired
     private lateinit var getTeamsService: GetTeamsService
+
+    @MockBean
+    private lateinit var findTeams: FindTeams
+
+    @MockBean
+    private lateinit var teamDao: TeamDao
 
 
     @Test
